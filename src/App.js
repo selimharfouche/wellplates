8667db446db1806cd0680a46a52b5f67779fc382
Selim Harfouche <selimharfouche@gmail.com>
2024-06-17 18:06:58 +0300

    react implementation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WellplatesSearch from './components/WellplatesSearch';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WellplatesSearch />
+    </div>
+  );
+}
+
+export default App;
