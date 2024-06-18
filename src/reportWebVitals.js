5b83a5e8ffcf81ca97914ff74d1a21fa663f9d4b
Selim Harfouche <selimharfouche@gmail.com>
2024-06-18 11:25:41 +0300

    basic react app

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,8 +1,6 @@
-// src/reportWebVitals.js
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB 
-}) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -13,4 +11,3 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
-
