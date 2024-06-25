97d229a88fe0661dcb9eadb1730b6d9b023b7acf
Selim Harfouche <selimharfouche@gmail.com>
2024-06-25 11:18:26 +0300

    more comments

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -6,15 +6,19 @@
 // It manages the search query state, handles input changes, and renders a list of filtered wellplates with links to their detail pages.
 //
 // ## References:
-//
+// * https://react.dev/reference/react/useState
 // * https://reactjs.org/docs/hooks-state.html
-// * https://reactrouter.com/web/api/Link
+
 
 // Import necessary libraries and components
 import React, { useState } from "react";
 import SearchInput from './SearchInput';
 import FilterSelect from './FilterSelect';
 import FilteredList from './FilteredList';
+import filters from '../../data/filters.json';//to be used later on, 
+//goal is to import the first set of filters, 
+//annd then generate dynamically the rest of the filters
+// thus saving time from iterating over the whole database for the first set of filters
 
 /**
  * The Search component provides a search bar and filters to filter through the wellplates data.
@@ -22,11 +26,20 @@ import FilteredList from './FilteredList';
  * @param {Array} data - Array of wellplate objects passed as a prop
  */
 const Search = ({ data }) => {
+  
+  /** 
+   * useState
+   * The useState hook returns an array with two elements:
+   * 1. The current state value
+   * 2. A function that lets us update the state value
+   */
   const [query, setQuery] = useState("");
   const [selectedMaterial, setSelectedMaterial] = useState("");
   const [selectedBrand, setSelectedBrand] = useState("");
   const [selectedNumberOfWells, setSelectedNumberOfWells] = useState("");
 
+  // handleChange, handleSomethingChange
+  // event handler functions that will be called when the input element changes
   const handleChange = (e) => {
     setQuery(e.target.value);
   };
@@ -43,6 +56,7 @@ const Search = ({ data }) => {
     setSelectedNumberOfWells(e.target.value);
   };
 
+  // Filters the data array based on the current values of query, selectedMaterial, selectedBrand and selectedNumberWells
   const filteredData = data.filter((item) => {
     return (
       item.name.toLowerCase().includes(query.toLowerCase()) &&
@@ -53,6 +67,7 @@ const Search = ({ data }) => {
   });
 
   // Generate dynamic options for remaining filters based on current selection
+  // more in the documentation
   const availableMaterials = [...new Set(data.filter(item => 
     (!selectedBrand || item.brand === selectedBrand) && 
     (!selectedNumberOfWells || item.number_of_wells === parseInt(selectedNumberOfWells))
