b2736bda695e5c2faab85de3f8c015bb487a6ffd
selimharfouche <57585478+selimharfouche@users.noreply.github.com>
2024-07-03 14:10:30 +0300

    Update Header.js

--- apps/client/src/components/Header.js
+++ apps/client/src/components/Header.js
@@ -2,9 +2,6 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom'; // Ensure you have react-router-dom installed
 
 const Header = () => {
